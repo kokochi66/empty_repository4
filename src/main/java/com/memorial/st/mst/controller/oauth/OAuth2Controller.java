1bc315b9d1563e81e89bbcf9dc0c7f0c7193b616
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-04-18 01:38:43 +0900

    Response 변경

--- src/main/java/com/memorial/st/mst/controller/oauth/OAuth2Controller.java
+++ src/main/java/com/memorial/st/mst/controller/oauth/OAuth2Controller.java
@@ -1,14 +1,9 @@
 package com.memorial.st.mst.controller.oauth;
 
+import com.memorial.st.mst.controller.oauth.model.AccessTokenResponse;
 import com.memorial.st.mst.domain.client.service.OAuth2TokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.token.TokenService;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,14 +15,14 @@ public class OAuth2Controller {
 
 
     @PostMapping("/token")
-    public ResponseEntity<?> token(@RequestParam("grant_type") String grantType,
-                                   @RequestParam("scope") String scope,
-                                   @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) throws Exception {
+    public AccessTokenResponse token(@RequestParam("grant_type") String grantType,
+                                     @RequestParam("scope") String scope,
+                                     @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) throws Exception {
         String[] clientCredentials  = oAuth2TokenService.decodeClientCredentials(authorization);
         String clientId = clientCredentials[0];
         String clientSecret = clientCredentials[1];
 
-        return new ResponseEntity<>(oAuth2TokenService.issueAccessToken(grantType, clientId, clientSecret, scope));
+        return oAuth2TokenService.issueAccessToken(grantType, clientId, clientSecret, scope);
     }
 
 }
