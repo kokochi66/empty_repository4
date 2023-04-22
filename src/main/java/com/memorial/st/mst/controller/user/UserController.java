801f7bd00eb9a6a224b26806c61325a34c5099f6
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-04-23 00:24:35 +0900

    UserConsent API 테스트 코드 작성

--- src/main/java/com/memorial/st/mst/controller/user/UserController.java
+++ src/main/java/com/memorial/st/mst/controller/user/UserController.java
@@ -8,6 +8,7 @@ import com.memorial.st.mst.domain.user.service.UserService;
 import com.memorial.st.mst.interceptor.AuthExcludes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,27 +30,27 @@ public class UserController {
     // 로그인
     @AuthExcludes
     @PostMapping("/login")
-    public Boolean login(@RequestBody MstUserRequest request, HttpServletResponse response) throws Exception {
+    public ResponseEntity<Boolean> login(@RequestBody MstUserRequest request, HttpServletResponse response) throws Exception {
         try {
             String jwtToken = userService.userLogin(request.getUserId(), request.getPassword());
             response.setHeader("Authorization", "Bearer " + jwtToken); // 표준화된 형식에 맞게 JWT 토큰을 Authorization 헤더에 담기
-            return true;
+            return ResponseEntity.ok(true);
         } catch (Exception e) {
-            return false;
+            return ResponseEntity.ok(false);
         }
     }
 
     // 회원가입
     @AuthExcludes
     @PostMapping("/register")
-    public MstUserResponse register(@RequestBody MstUserRequest request, HttpServletResponse response) throws Exception {
-        return new MstUserResponse(userService.register(request.getUserName(), request.getNickName(), request.getPassword(), request.getRole()));
+    public ResponseEntity<MstUserResponse> register(@RequestBody MstUserRequest request, HttpServletResponse response) throws Exception {
+        return ResponseEntity.ok(new MstUserResponse(userService.register(request.getUserName(), request.getNickName(), request.getPassword(), request.getRole())));
     }
 
     // 사용자 정보 동의
     @PostMapping("/consent")
-    public Boolean createConsent(@RequestBody UserConsentRequest consentRequest) {
+    public ResponseEntity<Boolean> createConsent(@RequestBody UserConsentRequest consentRequest) {
         userConsentService.createConsent(consentRequest);
-        return true;
+        return ResponseEntity.ok(true);
     }
 }
--- src/test/java/com/memorial/st/mst/controller/user/UserControllerTest.java
+++ src/test/java/com/memorial/st/mst/controller/user/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.memorial.st.mst.controller.user;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.memorial.st.mst.controller.user.model.UserConsentRequest;
 import com.memorial.st.mst.domain.user.MstRole;
 import com.memorial.st.mst.domain.user.MstUser;
 import com.memorial.st.mst.domain.user.service.UserService;
@@ -13,13 +14,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.sql.Date;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-public class UserTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
 
     @Autowired
     private UserService userService;
@@ -33,6 +40,9 @@ public class UserTest {
 
     @Value("${auth.project.id}")
     private String projectId;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @BeforeEach
     public void setUp() {
@@ -71,5 +81,27 @@ public class UserTest {
 
         // 토큰 만료 시간 확인 (1시간 이내)
         assertTrue(decodedJWT.getExpiresAt().after(new Date(System.currentTimeMillis())));
+    }
+
+    @Test
+    public void createConsentTest() {
+        // 테스트 데이터 생성
+        UserConsentRequest consentRequest = new UserConsentRequest();
+        consentRequest.setUserId(1L);
+        consentRequest.setClientEntityId("test-client-id");
+        List<String> scopes = Arrays.asList("read", "write");
+        consentRequest.setScopes(scopes);
+
+        // 사용자 정보 동의 요청 테스트
+        ResponseEntity<Boolean> response = restTemplate.postForEntity("/user/consent", consentRequest, Boolean.class);
+
+        // 응답 상태 및 본문 확인
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(true);
+
+        // 동의 내역 확인 (예: 데이터베이스에서 조회)
+        // 이 부분은 서비스에서 동의 정보를 어떻게 저장하고 검색하는지에 따라 구현이 달라집니다.
+        // List<String> consentScopes = userConsentService.findConsentScopes(consentRequest.getUserId(), consentRequest.getClientId());
+        // assertThat(consentScopes).containsAll(scopes);
     }
 }
