4164cfcaff60ef88379da0d100e10cc11f6f1942
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-28 19:21:32 +0900

    StudyFlim으로 리파지토리 이동, 정리

--- src/main/java/com/memorial/st/mst/domain/user/MstRole.java
+++
@@ -1,6 +0,0 @@
-package com.memorial.st.mst.domain.user;
-
-public enum MstRole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- src/main/java/com/memorial/st/mst/domain/user/service/repository/MstUserConsentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.memorial.st.mst.domain.user.service.repository;
-
-import com.memorial.st.mst.domain.user.MstUserConsent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface MstUserConsentRepository extends JpaRepository<MstUserConsent, Long> {
-}
